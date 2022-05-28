363f320568b6cfcef78773854637bed1e50860c4
Somabree <104927249+Somabree@users.noreply.github.com>
2022-05-29 00:45:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] 
+    sorted_word = sorted(word)
+    sorted_anagram = sorted(anagram)
+
+    if sorted_word == sorted_anagram:
+        return True
+
+    else:
+        return False
+
+print(find_anagram("same", "seam"))
+
+
+
